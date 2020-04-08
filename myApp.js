63a93d4c0ea7195bcccdd48a514fe6f52ac155a2
kTasuja <kaiomartin0@gmail.com>
2020-04-08 15:58:43 +0100

    Connect DB

--- myApp.js
+++ myApp.js
@@ -13,8 +13,9 @@
 // as MONGO_URI. Connect to the database using the following syntax:
 //
 // mongoose.connect(<Your URI>, { useNewUrlParser: true, useUnifiedTopology: true });
-
+const dotenv = require("dotenv");
 const mongoose = require("mongoose");
+dotenv.config();
 mongoose.connect(process.env.MONGO_URI, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
